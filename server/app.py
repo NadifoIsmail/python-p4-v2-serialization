377afd677175ffe08be5e8a39a7efcdeadd6d763
Nadifo Ismail <nadifoismail80@gmail.com>
2025-02-05 09:34:33 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,3 @@
-# server/app.py
-#!/usr/bin/env python3
-
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
@@ -19,6 +16,19 @@ db.init_app(app)
 def index():
     body = {'message': 'Welcome to the pet directory!'}
     return make_response(body, 200)
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        body = pet.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Pet {id} not found.'}
+        status = 404
+
+    return make_response(body, status)    
 
 
 if __name__ == '__main__':
